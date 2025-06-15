690653d9d40c08beeb8d468155ffc0028681a8f0
AethiQs-Karel <karel.hernandez@aethiqs.nl>
2025-06-15 20:08:51 -0300

    feat: implement GET and POST endpoints for products; handle errors and return appropriate responses

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -1,9 +1,47 @@
 import { NextResponse } from "next/server";
+import { prisma } from "@/libs/prisma";
 
 export async function GET() {
-  return NextResponse.json({ message: "Hello World" });
+  try {
+    const products = await prisma.product.findMany();
+    return NextResponse.json(products);
+  } catch (error) {
+    console.error("Error fetching products:", error);
+    return NextResponse.json(
+      { error: "Failed to fetch products" },
+      { status: 500 }
+    );
+  }
 }
 
-export async function POST() {
-  return NextResponse.json({ message: "Hello World" });
+export async function POST(request) {
+  try {
+    const { name, description, price } = await request.json();
+    if (!name) {
+      return NextResponse.json({ message: "Invalid request" });
+    }
+    const product = await prisma.product.create({
+      data: {
+        name,
+        description,
+        price,
+      },
+    });
+    if (!product) {
+      return NextResponse.json(
+        { message: "Failed to create product" },
+        { status: 500 }
+      );
+    }
+    return NextResponse.json({
+      message: "Product created successfully",
+      product,
+    });
+  } catch (error) {
+    console.error("Error creating product:", error);
+    return NextResponse.json(
+      { error: "Failed to create product" },
+      { status: 500 }
+    );
+  }
 }
